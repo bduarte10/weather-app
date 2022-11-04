5851418b3ed5543fb5e70fc6f88c42b715d5c5b5
Bruno Duarte <duartebruno2208@gmail.com>
2022-11-04 13:49:05 -0300

    eventlistener

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+//variáveis e seleção de elementos
+const apiWeather = '1bf3ab693706ad462427be4a28b9baee'
+const countryflagsapi = 'https://countryflagsapi.com/png/'
 
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search-btn')
+
+//funções
+
+//eventos
+
+searchBtn.addEventListener('click', () => {
+  console.log(cityInput.value)
+})
